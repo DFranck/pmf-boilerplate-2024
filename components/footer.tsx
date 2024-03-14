b4504815bf59cec5c4c21b26df8458d1c1be3d1d
DUFOURNET Franck <franckdufournet@hotmail.fr>
2024-03-14 13:33:32 +0700

    Footer minimal style

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,5 +1,14 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const Footer = () => {
-  return <div>Footer content</div>;
+  return (
+    <footer className="flex w-full items-center justify-center border-t-2 border-black p-10">
+      <Link href="/">
+        <Image src="/next.svg" alt="logo" width={175} height={175} />
+      </Link>
+    </footer>
+  );
 };
 
 export default Footer;
