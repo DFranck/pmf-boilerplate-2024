6405fbcb31c459e1304b2e2ac499028ae7455212
DUFOURNET Franck <franckdufournet@hotmail.fr>
2024-03-14 13:31:25 +0700

    header minimal style

--- components/header.tsx
+++ components/header.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { signOut, useSession } from "next-auth/react";
+import Image from "next/image";
+import Link from "next/link";
 import { Button } from "./ui/button";
 
 const Header = () => {
@@ -8,13 +10,26 @@ const Header = () => {
   console.log(session?.user);
 
   return (
-    <header>
-      {session?.user && <p>Bonjour {session.user.username}</p>}
-      {session?.user ? (
-        <Button onClick={() => signOut()}>Deconnexion</Button>
-      ) : (
-        <Button>Connexion</Button>
-      )}
+    <header className=" p-5 border-b-2 border-black">
+      <div className="flex justify-between max-w-7xl mx-auto">
+        <Link className="rounded-full" href="/">
+          <Image
+            className="rounded-full"
+            src="/next.svg"
+            alt="logo"
+            width={75}
+            height={75}
+          />
+        </Link>
+        <div className="flex justify-center items-center gap-5">
+          {session?.user && <p>Bonjour {session.user.username}</p>}
+          {session?.user ? (
+            <Button onClick={() => signOut()}>Deconnexion</Button>
+          ) : (
+            <Button>Connexion</Button>
+          )}
+        </div>
+      </div>
     </header>
   );
 };
